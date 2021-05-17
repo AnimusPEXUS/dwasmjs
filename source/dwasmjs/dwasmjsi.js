f485735b9ffd0ce5df309b452da075d300cec50f
Animus PEXUS <animuspexus@protonmail.com>
2021-05-17 19:39:31 +0300

    ic

--- source/dwasmjs/dwasmjsi.js
+++ source/dwasmjs/dwasmjsi.js
@@ -1,25 +1,30 @@
+console.log("dwasmjsi.js start");
 (() => {
+
+    const debug = true
+    const debug_log = (txt) => {
+        if (debug) {
+            console.log(txt);
+        }
+    }
 
     const utf8encoder = new TextEncoder('utf-8');
     const utf8decoder = new TextDecoder('utf-8');
 
-    const memGetInt32 = (addr) => {
-        this.memory
-    }
-
-    const memGetString = (addr, len) => {
-        var buffer = new Uint8Array(this.memory.buffer, ptr, len);
-        var msg = utf8decoder.decode(buffer);
-        return msg;
-    }
+    // const memGetInt32 = (addr) => {
+    //     this.memory
+    // }
 
 
-    global.DWasmJSCode = class {
-        constructor(filename) {
+    globalThis.DWasmJSEnvironment = class {
+        constructor() {
+            debug_log("constructor called");
 
             this._js_value_storage = new Map();
             this._id_by_value = new Map();
-            this._released_id_pool = = new Set();
+            this._released_id_pool = new Set();
+
+            debug_log("lists created");
 
             this._predefined_values = [
                 undefined,
@@ -27,38 +32,49 @@
                 false,
                 true,
                 NaN,
-                infinity,
+                // infinity,
                 globalThis,
-                this,
+                // this,
             ];
 
-            this._js_value_storage_id_counter = this._predefined_values.size;
+            debug_log("predefined values inserted");
 
-            for (let i = 0; i != this._predefined_values.size; i++) {
+            this._js_value_storage_id_counter = this._predefined_values.length;
+
+            for (let i = 0; i != this._predefined_values.length; i++) {
+                debug_log(i);
                 this._js_value_storage[i] = this._predefined_values[i];
                 this._id_by_value[this._predefined_values[i]] = i;
             }
 
+            debug_log("populated _js_value_storage and _id_by_value");
+
             const newValueId = (v) => {
-                if (this._released_id_pool.size != 0) {
+                var ret;
+                debug_log("new value_id requested");
+                if (this._released_id_pool.length != 0) {
+                    debug_log("  reusing released");
                     ret = this._released_id_pool[0];
                     delete this._released_id_pool[0];
-                    return ret;
                 } else {
+                    debug_log("  allocating new");
                     ret = this._js_value_storage_id_counter;
                     this._js_value_storage_id_counter++;
-                    return ret;
                 }
+                return ret;
             }
 
             const storeJSValue = (v) => {
+                debug_log("storing value");
                 value_id = newValueId(v);
                 this._js_value_storage[value_id] = v;
+                debug_log(" stored as " + string(v));
                 return value_id;
             };
 
             const releaseJSValue = (value_id) => {
-                if (value_id < this._predefined_values.size) {
+                debug_log("releasing value " + string(value_id));
+                if (value_id < this._predefined_values.length) {
                     throw new Exception("value_id value is too low");
                 }
                 try {
@@ -75,48 +91,72 @@
                 return this._js_value_storage[value_id];
             }
 
+            const memGetString = (addr, len) => {
+                var buffer = new Uint8Array(
+                    this.instance.exports.memory.buffer,
+                    addr,
+                    len
+                );
+                var msg = utf8decoder.decode(buffer);
+                return msg;
+            }
+
             this.importObject = {
-                'dwasmjsclass_reflect_apply': (sp) => {
-                    sp >>>= 0;
+                'env': {
+                    'dwasmjsclass_console_log': (addr, length) => {
+                        console.log(memGetString(addr, length))
+                        return;
+                    },
                 },
-                'dwasmjsclass_reflect_construct': (sp) => {
-                    sp >>>= 0;
-                },
-                'dwasmjsclass_reflect_deleteProperty': (sp) => {
-                    sp >>>= 0;
-                },
-                'dwasmjsclass_reflect_has': (sp) => {
-                    sp >>>= 0;
-                },
-                'dwasmjsclass_reflect_get': (sp) => {
-                    sp >>>= 0;
-                },
-                'dwasmjsclass_reflect_set': (sp) => {
-                    sp >>>= 0;
-                },
+                // 'dwasmjsclass_reflect_apply': (sp) => {
+                //     sp >>>= 0;
+                // },
+                // 'dwasmjsclass_reflect_construct': (sp) => {
+                //     sp >>>= 0;
+                // },
+                // 'dwasmjsclass_reflect_deleteProperty': (sp) => {
+                //     sp >>>= 0;
+                // },
+                // 'dwasmjsclass_reflect_has': (sp) => {
+                //     sp >>>= 0;
+                // },
+                // 'dwasmjsclass_reflect_get': (sp) => {
+                //     sp >>>= 0;
+                // },
+                // 'dwasmjsclass_reflect_set': (sp) => {
+                //     sp >>>= 0;
+                // },
+                //
+                // 'dwasmjsclass_value_get_type': (sp) => {
+                //     sp >>>= 0;
+                //     return 123;
+                // },
 
-                'dwasmjsclass_value_get_type': (sp) => {
-                    sp >>>= 0;
-                    return 123;
-                },
-
-                'dwasmjsclass_console_log': (addr, length) => {
-                    console.log(memGetString(addr, length))
-                    return ;
-                },
+                // 'dwasmjsclass_console_log2': (addr, length) => {
+                //     console.log(memGetString(addr, length))
+                //     return;
+                // },
             };
 
-            WebAssembly.instantiateStreaming(
-                fetch(filename),
-                this.importObject,
-            ).then(
-                (result) => {
-                    this.instance = result.instance;
-                    this.memory = new DataView(this.instance.exports.mem.buffer);
-                }
-            );
+            debug_log("importObject populated");
+        }
 
+        setResult(result) {
+            this.module = result.module;
+            this.instance = result.instance;
+            // this.memory = new DataView(this.instance.exports.memory.buffer);
+        }
+
+        async useResult(result) {
+            debug_log("setResult");
+            this.setResult(result);
+            debug_log("run")
+            const {
+                exports
+            } = result.instance;
+            exports.run();
         }
     }
-
+    console.log("globalThis.DWasmJSCode defined now");
 })();
+console.log("dwasmjsi.js end");
--- source/dwasmjs/js.d
+++ source/dwasmjs/js.d
@@ -1,39 +1,40 @@
-
 alias value_id = size_t;
 
 extern (C)
 {
-
-    int dwasmjsclass_value_get_type();
-
     void dwasmjsclass_console_log(immutable(char)* str, size_t len);
+}
+
+extern
+{
 }
 
 /* extern{
     _D6object6Object8toStringMFZAya
 } */
 
-
 void consoleLog(string txt)
 {
     dwasmjsclass_console_log(txt.ptr, txt.length);
 }
 
-
-class Value {
+class Value
+{
 
     private value_id id;
     private bool value_have_to_be_released = false;
 
-
-    this() {
+    this()
+    {
     }
 
-    this(value_id id) {
+    this(value_id id)
+    {
         this.id = id;
     }
 
-    ~this() {
+    ~this()
+    {
         if (value_have_to_be_released)
         {
 
